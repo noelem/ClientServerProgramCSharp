3063be166398feb6daa4ce58ef3613c339738c0c
Henrik Bulskov <bulskov@ruc.dk>
2021-10-19 11:25:19 +0200

    Start creating the client

--- Client/ClientProgram.cs
+++ Client/ClientProgram.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Sockets;
 
 namespace Client
 {
@@ -6,7 +7,14 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var client = new TcpClient();
+
+            client.Connect("localhost", 5000);
+
+
+            
+ 
         }
     }
 }
