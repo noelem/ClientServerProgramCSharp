b263705a91aef2397a6a5d87948f5fe56a1d1d82
Henrik Bulskov <bulskov@ruc.dk>
2021-10-19 13:38:14 +0200

    Change the extension to read input of arbitrary sizes

--- Utillities/TcpCleintExtensions.cs
+++ Utillities/TcpCleintExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using System.Text;
 
@@ -11,11 +12,26 @@ namespace Utillities
             client.GetStream().Write(Encoding.UTF8.GetBytes(message));
         }
 
-        public static string Receive(this TcpClient client)
+        public static string Receive(this TcpClient client, int bufferSize = 1024)
         {
-            var buffer = new byte[1024];
-            var rdCnt =client.GetStream().Read(buffer);
-            return Encoding.UTF8.GetString(buffer, 0, rdCnt);
+            //var buffer = new byte[1024];
+            //var rdCnt =client.GetStream().Read(buffer);
+            //return Encoding.UTF8.GetString(buffer, 0, rdCnt);
+
+            var strm = client.GetStream();
+            byte[] resp = new byte[bufferSize];
+            using (var memStream = new MemoryStream())
+            {
+                int bytesread = 0;
+                do
+                {
+                    bytesread = strm.Read(resp, 0, resp.Length);
+                    memStream.Write(resp, 0, bytesread);
+
+                } while (bytesread == bufferSize);
+
+                return Encoding.UTF8.GetString(memStream.ToArray());
+            }
         }
     }
 }
