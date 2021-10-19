241accbb685f515b5e1856702ae5b9532d632274
Henrik Bulskov <bulskov@ruc.dk>
2021-10-19 13:14:06 +0200

    Add utillities project with TcpClient extensions

---
+++ Utillities/TcpCleintExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Utillities
+{
+    public static class TcpCleintExtensions
+    {
+        public static void Send(this TcpClient client, string message)
+        {
+            client.GetStream().Write(Encoding.UTF8.GetBytes(message));
+        }
+
+        public static string Receive(this TcpClient client)
+        {
+            var buffer = new byte[1024];
+            var rdCnt =client.GetStream().Read(buffer);
+            return Encoding.UTF8.GetString(buffer, 0, rdCnt);
+        }
+    }
+}
